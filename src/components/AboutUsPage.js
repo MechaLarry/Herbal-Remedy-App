0a83afb02c4ce3da65ea5897fce525df4f3a6aa1
Larry Mecha <122802255+MechaLarry@users.noreply.github.com>
2024-08-20 07:57:12 +0300

    Update AboutUsPage.js
    add deployed site link

--- src/components/AboutUsPage.js
+++ src/components/AboutUsPage.js
@@ -5,7 +5,7 @@ function AboutUsPage() {
     <div className="about-us">
       <h2>About Us</h2>
       <p>We are about giving remedies for treatments that give people relief. 
-      Our prescribed products are harmless to the body and user friendly.</p>
+      Our prescribed products are harmless to the body and user friendly. Catch us on "https://mechalarry.github.io/Herbal-Remedy-App/" </p>
     </div>
   );
 }
